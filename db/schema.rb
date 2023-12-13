b0b8c186081852f886fe0b864c7a50f65228096e
yutaiwahashi <yuta.iwahahsi@gmail.com>
2023-12-14 03:44:44 +0900

    user teble create

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_13_033323) do
+ActiveRecord::Schema.define(version: 2023_12_13_183924) do
 
   create_table "prototypes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -20,9 +20,20 @@ ActiveRecord::Schema.define(version: 2023_12_13_033323) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "prptptypes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
+    t.text "profile", null: false
+    t.text "occupation", null: false
+    t.text "position", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
